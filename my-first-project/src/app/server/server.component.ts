da56b2c420a1a2adf93ae869e4c4f0689c9c6ee7
Suresh Yerrarapu <yerrarapusuresh@gmail.com>
2020-02-14 00:54:27 +0530

    added my first dynamic variable

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styleUrls:['./server.component.css']
 })
 export class ServerComponent {
+    serverNumber: number = 10;
+    serverStatus: string = 'Offline';
 
+    isAllowed:boolean = false;
+    constructor(){
+        setTimeout(() =>{
+            this.isAllowed = true;
+        }, 2000);
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
